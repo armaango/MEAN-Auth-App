d22b95cbbd670ed68fe77ba9a2b2a75d9f616f9a
armaango <armaan.goyal@gmail.com>
2017-02-25 23:06:04 -0800

    mongoose and user routes setup

--- app.js
+++ app.js
@@ -4,11 +4,36 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
+
+//connect to db
+mongoose.connect(config.database);
+
+//on connection
+mongoose.connection.on('connected',()=>{
+    console.log('Connected to db'+config.database);
+});
+
+//on error
+mongoose.connection.on('error',(err)=>{
+    console.log('Database error:'+ err);
+});
 
 const app = express();
 
+const users = require('./routes/users');
+
 const port = 3000;
+//CORS middleware
 app.use(cors());
+
+//Set Static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+//Body parser middleware
+app.use(bodyParser.json());
+
+app.use('/users',users);
 
 app.get('/',(req,res)=>{
     res.send('Invalid endpoint');
