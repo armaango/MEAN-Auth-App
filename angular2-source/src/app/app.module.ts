603ac65e0c7041154330278ccfaf9cd1ca0c055c
armaango <armaan.goyal@gmail.com>
2017-03-01 00:11:58 -0800

    Added Register component and validations

--- angular2-source/src/app/app.module.ts
+++ angular2-source/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -12,23 +12,25 @@ import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
-const appRoutes:Routes=[
+import {ValidateService} from './services/validate.service';
+import {FlashMessagesModule} from 'angular2-flash-messages';
+const appRoutes: Routes = [
   {
-    path:'',
-    component:HomeComponent
+    path: '',
+    component: HomeComponent
   },
   {
-    path:'register',
-    component:RegisterComponent
-  },{
-    path:'login',
-    component:LoginComponent
-  },{
-    path:'dashboard',
-    component:DashboardComponent
-  },{
-    path:'profile',
-    component:ProfileComponent
+    path: 'register',
+    component: RegisterComponent
+  }, {
+    path: 'login',
+    component: LoginComponent
+  }, {
+    path: 'dashboard',
+    component: DashboardComponent
+  }, {
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 
@@ -46,9 +48,10 @@ const appRoutes:Routes=[
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
