892794fc33d39d5ce8f0dda4fcba274e85463e5b
armaango <armaan.goyal@gmail.com>
2017-03-01 01:31:04 -0800

    links hidden on login/logout

--- angular2-source/src/app/services/auth.service.ts
+++ angular2-source/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -46,4 +47,8 @@ export class AuthService {
     this.user = null;
     localStorage.clear();
   }
+  loggedIn() {
+  return tokenNotExpired();
+}
+
 }
